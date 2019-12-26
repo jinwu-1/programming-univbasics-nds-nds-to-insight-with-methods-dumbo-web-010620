f967b7414675068c1b586612f2529cc7b152b172
Jin Wu <jinn.wuu@gmail.com>
2019-12-26 20:53:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ require 'directors_database'
 def gross_for_director(director_data)
   movie_index = 0
   total_gross = 0
-  while movie_index < director_data[:movies].length.do
+  while movie_index < director_data[:movies].length do
     total_gross += director_data[:movies][movie_index][:worldwide_grosses]
     movie_index += 1
   end
